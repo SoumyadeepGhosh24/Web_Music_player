519a125c2863b927129a108c76e028bee4cd4b57
Soumyadeep Ghosh <76256823+SoumyadeepGhosh24@users.noreply.github.com>
2023-11-12 23:10:57 +0530

    Add files via upload

---
+++ music_list.js
@@ -0,0 +1,86 @@
+let allMusic = [
+    {
+        name: "On my Way",
+        artist: "Alan Walker",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Obosheshe",
+        artist: "Arijit Singh",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Baby don't want you",
+        artist: "Alec Benjamin",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Chaleya",
+        artist: "Ellie Goulding",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Kaavaalaa",
+        artist: "The Weeknd",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Zinda Banda",
+        artist: "Maroon 5",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Not Ramaiya Vastavaiya",
+        artist: "Alan Walker",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Namo Namo",
+        artist: "Amit",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Tu Aake Dekhle",
+        artist: "King",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "O Saathi",
+        artist: "Atif Aslam",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Heeriye",
+        artist: "Arijit Singh",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Ghawrbaari",
+        artist: "Anupam Roy",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Mera Safar",
+        artist: "Iqlipse Nova",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Dil Diyan Gallan",
+        artist: "Atf Aslan",
+        img: "music-14",
+        src: "music-14"
+    },
+];
